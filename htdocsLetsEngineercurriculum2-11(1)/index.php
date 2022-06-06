2c614fcd77eb1ed521da105bfa246b260c757600
yasukawa-hiromi <93073778+hiromi-yaskawa@users.noreply.github.com>
2022-06-06 22:02:40 +0900

    Add files via upload

---
+++ htdocsLetsEngineercurriculum2-11(1)/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>入力フォーム</title>
+	<link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+<div>
+<form action="result.php"method="post">
+名前:<input type="text" name="my_name"/>
+<br>
+メールアドレス:<input type="email" name="email"/>
+<br>
+パスワード:<input type="password" name="password"/>
+<br>
+<input type="submit" value="送信"/>
+</form>
+</div>
+</body>
+</html>
